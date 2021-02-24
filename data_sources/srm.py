c69cab9768d398fc1d72aa86e1d0e948f0e94ae6
Mike <mike.hukiewitz@wechselgott.com>
2021-02-24 15:13:51 +0100

    implement SerumMetricProvider

--- data_sources/srm.py
+++ data_sources/srm.py
@@ -1,5 +1,8 @@
+from typing import Union
+
 from data_sources.apicalls import retrieve_json
-from data_sources.metrics import DexMetricProvider
+from data_sources.metrics import DexMetricProvider, ChainMetricProvider
+from data_sources.sol import SolanaMetricProvider
 
 
 def get_all_pools():
@@ -8,14 +11,27 @@ def get_all_pools():
 
 
 class SerumMetricProvider(DexMetricProvider):
-    def __init__(self):
-        super().__init__("Serum")
+    pools_resp: dict
 
-    def get_estimated_gas(self) -> int:
-        pass
+    def __init__(self, chain: ChainMetricProvider = SolanaMetricProvider()):
+        super().__init__("SerumSwap", chain, "https://swap.projectserum.com/")
+        self.refresh()
+
+    def refresh(self):
+        self.pools_resp = get_all_pools()
+
+    def get_estimated_gas(self) -> Union[None, int]:
+        return self.chain.get_avg_txn_gas()
 
     def get_minimum_maximum_apy(self) -> (float, float):
-        pass
+        raise NotImplemented
 
-    def get_current_tvl(self) -> float:
-        pass
+    def get_current_tvl(self) -> Union[None, float]:
+        if self.pools_resp['success'] == 'false':
+            return None
+
+        tvl = 0
+        for pool in self.pools_resp['data']:
+            tvl += pool['liquidity_locked']
+        return round(tvl, 2)
+
