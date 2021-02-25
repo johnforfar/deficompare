9cfd5603531d0e84ea64297ecdd8f0c819d6d1a8
Gavin <gavinjboyd@gmail.com>
2021-02-26 00:22:17 +0900

    refactor to token metrics service

--- database.py
+++ database.py
@@ -3,10 +3,12 @@
 import sqlite3
 import pandas as pd
 
-from constants import TOKEN_CODES
-from metrics_service import MetricsService
+from constants import DB_NAME, TOKEN_METRICS_SUFFIX, EXCHANGE_METRICS_SUFFIX, UNISWAP_EXCHANGE_CODE, SOLANA_TOKEN_CODE, \
+    ETHERIUM_TOKEN_CODE
+from exchange_metrics_service import ExchangeMetricsService
+from token_metrics_service import TokenMetricsService
 
-DB_NAME = 'db.db'
+
 
 
 class SQLLiteDatabase:
@@ -15,16 +17,16 @@ class SQLLiteDatabase:
 
         print(f"Opened {DB_NAME} successfully")
 
-        # Create a table for each token
-        for token_code in TOKEN_CODES:
-            self.conn.execute(f'''CREATE TABLE IF NOT EXISTS {token_code}_metrics 
-                     (id INT PRIMARY KEY     NOT NULL,
-                     timestamp TIMESTAMP CURRENT_TIMESTAMP,
-                     tx_fee            DOUBLE     NOT NULL,
-                     tx_delay            DOUBLE     NOT NULL
-                     );''')
-
-        self.conn.close()
+        # # Create a table for each token
+        # for token_code in TOKEN_CODES:
+        #     self.conn.execute(f'''CREATE TABLE IF NOT EXISTS {token_code}_metrics
+        #              (id INT PRIMARY KEY     NOT NULL,
+        #              timestamp TIMESTAMP CURRENT_TIMESTAMP,
+        #              tx_fee            DOUBLE     NOT NULL,
+        #              tx_delay            DOUBLE     NOT NULL
+        #              );''')
+        #
+        # self.conn.close()
 
     def get_token_df(self, token_code) -> pd.DataFrame:
         """"""
@@ -37,15 +39,33 @@ class SQLLiteDatabase:
         self.conn.close()
         return df
 
+    def get_exchange_df(self, exchange_code) -> pd.DataFrame:
+        """"""
+        self.conn = sqlite3.connect(DB_NAME)
+
+        print(f"Opened {DB_NAME} successfully")
+
+        df = pd.read_sql_query(f"SELECT * from {exchange_code}_metrics", self.conn)
+
+        self.conn.close()
+        return df
+
     def store_dummy_data(self):
         """Just for development, storing test data"""
         self.conn = sqlite3.connect(DB_NAME)
 
-        metric_service = MetricsService(self)
-        eth_df = metric_service.get_dummy_data_eth()
-        eth_df.to_sql("eth_metrics", self.conn, if_exists="replace")
-        sol_df = metric_service.get_dummy_data_sol()
-        sol_df.to_sql("sol_metrics", self.conn, if_exists="replace")
+        # Add tokens - manually for now
+        token_metrics_service = TokenMetricsService(self)
+
+        eth_df = token_metrics_service.get_dummy_data_eth()
+        eth_df.to_sql(f"{ETHERIUM_TOKEN_CODE}{TOKEN_METRICS_SUFFIX}", self.conn, if_exists="replace")
+        sol_df = token_metrics_service.get_dummy_data_sol()
+        sol_df.to_sql(f"{SOLANA_TOKEN_CODE}{TOKEN_METRICS_SUFFIX}", self.conn, if_exists="replace")
+
+        exchange_metrics_service = ExchangeMetricsService(self)
+        uniswap_df = exchange_metrics_service.get_dummy_data_uniswap()
+        uniswap_df.to_sql(f"{UNISWAP_EXCHANGE_CODE}{EXCHANGE_METRICS_SUFFIX}", self.conn, if_exists="replace")
+
 
         self.conn.close()
 
