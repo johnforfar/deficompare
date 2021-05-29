b56b53d9fd8c63cd4d81918462522bce7cb5a87d
Gavin <gavinjboyd@gmail.com>
2021-05-30 08:31:45 +0900

    wip: add token metric comparison chart

---
+++ front-end/src/components/TokenMetricsComparisonChart/TokenMetricsComparisonChart.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./styles.scss";
+
+type ContainerProps = {};
+
+const TokenMetricsComparisonChart = (props: ContainerProps) => {
+  return <div className="container"></div>;
+};
+
+export default TokenMetricsComparisonChart;
