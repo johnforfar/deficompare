76af5d8320b27f80f6ea6ee755a74147c9ded1f7
Mike <mike.hukiewitz@wechselgott.com>
2021-05-31 10:37:05 +0200

    add hasura data insert for price and txn cost

--- data_sources/bitquery_example.py
+++ data_sources/bitquery_example.py
@@ -14,44 +14,22 @@ bitquery_to_cg_ids = {"ethereum": "ethereum",
                       "litecoin": "litecoin",
                       "bitcash": "bitcoin-cash",
                       "dash": "dash",
-                      "cardano": "cardano",
+                      #"cardano": "cardano",
                       "dogecoin": "dogecoin"}
 
-
-def get_eth_price_and_fees(since=None, till=None, interval=None):
-    return _get_eth_like_price_gas_fees("ethereum", since, till, interval)
-
-
-def get_bsc_price_and_fees(since=None, till=None, interval=None):
-    return _get_eth_like_price_gas_fees("bsc", since, till, interval)
+eth_likes = ["ethereum", "ethclassic", "bsc"]
+btc_likes = ["bitcash", "litecoin", "bitcash", "dash", "cardano", "dogecoin"]
 
 
-def get_etc_price_and_fees(since=None, till=None, interval=None):
-    return _get_eth_like_price_gas_fees("ethclassic", since, till, interval)
-
-
-def get_btc_price_and_fees(since=None, till=None, interval=None):
-    return _get_btc_like_price_gas_fees("bitcoin", since, till, interval)
-
-
-def get_ltc_price_and_fees(since=None, till=None, interval=None):
-    return _get_btc_like_price_gas_fees("litecoin", since, till, interval)
-
-
-def get_bch_price_and_fees(since=None, till=None, interval=None):
-    return _get_btc_like_price_gas_fees("bitcash", since, till, interval)
-
-
-def get_dash_price_and_fees(since=None, till=None, interval=None):
-    return _get_btc_like_price_gas_fees("dash", since, till, interval)
-
-
-def get_ada_price_and_fees(since=None, till=None, interval=None):
-    return _get_btc_like_price_gas_fees("cardano", since, till, interval)
-
-
-def get_doge_price_and_fees(since=None, till=None, interval=None):
-    return _get_btc_like_price_gas_fees("dogecoin", since, till, interval)
+def get_prices_and_fees(name, since=None, till=None, interval=None):
+    print(f"Getting {network} prices and fees...")
+    if name in eth_likes:
+        return _get_eth_like_price_gas_fees(name, since, till, interval)
+    elif name in btc_likes:
+        return _get_btc_like_price_gas_fees(name, since, till, interval)
+    else:
+        print(f"{name} does not exist.")
+        return None
 
 
 def _get_eth_like_price_gas_fees(network, since=None, till=None, interval=None):
@@ -64,17 +42,18 @@ def _get_eth_like_price_gas_fees(network, since=None, till=None, interval=None):
 
     gas_prices = get_average_eth_like_gas(network, since - interval, till, interval)
     usd_prices = get_historic_prices(bitquery_to_cg_ids[network], since, till)
+    if gas_prices is None or usd_prices is None:
+        return None
 
     binned_prices = usd_prices.resample(interval).mean().interpolate()
     binned_gas = gas_prices.resample(interval).mean().interpolate()
     binned_txn_fees = pd.Series({timestamp: price * (binned_gas[timestamp] / 1e9) * 65000  # GWEI <-> ETH conversion
-                                 for timestamp, price in binned_prices.iteritems()}, name="erc20_transfer")
+                                 for timestamp, price in binned_prices.iteritems()}, name="avg_txn_price")
 
-    binned_prices.name = "coin_price"
-    binned_gas.name = "gas_price"
-    df = pd.concat([binned_prices, binned_gas, binned_txn_fees], axis=1)
+    binned_prices.name = "avg_price"
+    df = pd.concat([binned_prices, binned_txn_fees], axis=1)
     df.name = network
-    return df
+    return df.assign(name=network)
 
 
 def _get_btc_like_price_gas_fees(network, since=None, till=None, interval=None):
@@ -87,33 +66,28 @@ def _get_btc_like_price_gas_fees(network, since=None, till=None, interval=None):
 
     coin_fees = get_average_btc_like_fees(network, since - interval, till, interval)
     usd_prices = get_historic_prices(bitquery_to_cg_ids[network], since, till)
+    if coin_fees is None or usd_prices is None:
+        return None
 
     binned_prices = usd_prices.resample(interval).mean().interpolate()
     binned_fees = coin_fees.resample(interval).mean().interpolate()
     binned_txn_fees = pd.Series({timestamp: price * binned_fees[timestamp]  # GWEI <-> ETH conversion
-                                 for timestamp, price in binned_prices.iteritems()}, name="transaction_cost")
+                                 for timestamp, price in binned_prices.iteritems()}, name="avg_txn_price")
 
-    binned_prices.name = "coin_price"
-    binned_fees.name = "coin_fee"
-    df = pd.concat([binned_prices, binned_fees, binned_txn_fees], axis=1)
+    binned_prices.name = "avg_price"
+    df = pd.concat([binned_prices, binned_txn_fees], axis=1)
     df.name = network
-    return df
+    return df.assign(name=network)
 
-#print(get_eth_price_and_fees())
-#print(get_bnb_price_and_fees())
-#print(get_etc_price_and_fees())
-#print(get_btc_price_and_fees())
-#print(get_ltc_price_and_fees())
-#print(get_bch_price_and_fees())
-#print(get_dash_price_and_fees())
-#print(get_ada_price_and_fees())
-#print(get_doge_price_and_fees())
+from data_sources.hasura import insert_network_coins
 
-# costs_etc = pd.Series(get_average_eth_like_gas("ethclassic", now - timedelta(hours=2), now, timedelta(minutes=15)), name="ETC")
-# costs_bsc = pd.Series(get_average_eth_like_gas("bsc", now - timedelta(hours=2), now, timedelta(minutes=15)), name="BNB")
-# costs_btc = pd.Series(get_average_btc_like_fees("bitcoin", now - timedelta(hours=2), now, timedelta(minutes=15)), name="BTC")
-# costs_doge = pd.Series(get_average_btc_like_fees("dogecoin", now - timedelta(hours=2), now, timedelta(minutes=15)), name="DOGE")
-# costs_ltc = pd.Series(get_average_btc_like_fees("litecoin", now - timedelta(hours=2), now, timedelta(minutes=15)), name="LTC")
-# series = [costs_eth, costs_etc, costs_bsc, costs_btc, costs_doge, costs_ltc]
-# df = pd.concat(series, axis=1)
-# print(df)
+now = datetime.now(tz=tz)
+till = now
+since = till - timedelta(hours=12)
+interval = timedelta(minutes=15)
+
+for network in bitquery_to_cg_ids.keys():
+    df = get_prices_and_fees(network, since, till, interval)
+    if df is None:
+        continue
+    insert_network_coins(df)
---
+++ data_sources/hasura.py
@@ -0,0 +1,40 @@
+import json
+import re
+
+from gql import gql, Client
+from gql.transport.requests import RequestsHTTPTransport
+
+import pandas as pd
+import codecs
+
+from data_sources.helpers import print_red
+from data_sources.keys import hasura_graphql_url, hasura_admin_secret
+
+hasura_client = Client(
+    transport=RequestsHTTPTransport(
+        url=hasura_graphql_url, headers={'content-type': 'application/json',
+                                         'x-hasura-admin-secret': hasura_admin_secret}))
+
+
+def insert_network_coins(df: pd.DataFrame):
+    df_records = df.to_dict(orient='index')
+    records = []
+    for key, values in df_records.items():
+        record = {"time": key.isoformat()}
+        record.update(values)
+        records.append(record)
+    records_str = re.sub(r'(?<!: )"(\S*?)"', '\\1', json.dumps(records))
+    print(records_str)
+    query = gql(
+        codecs.decode(rf"""mutation {{
+            insert_network_coins(objects: {records_str}) {{
+                returning {{
+                    time
+                    name
+                }}
+            }}
+        }}""", 'unicode_escape')
+    )
+    result = hasura_client.execute(query)
+    print(result)
+    return result
