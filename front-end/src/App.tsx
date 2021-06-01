f6bd0746e3ea1ceec91eceed9a5a1759a66c05ef
Gavin <gavinjboyd@gmail.com>
2021-06-01 21:24:42 +0900

    implement theme extend for chakra ui

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -2,36 +2,36 @@ import React from 'react';
 import './App.scss';
 import Navbar from './components/Navbar/Navbar';
 // 1. import `ChakraProvider` component
-import {Box, Button, ChakraProvider, useColorMode} from "@chakra-ui/react"
-import TimeScaleGraph from './components/TimeScaleGraph/TimeScaleGraph';
-import ComparisonTable from './components/ComparisonTable/ComparisonTable';
+import {ChakraProvider} from "@chakra-ui/react"
 import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
 } from "react-router-dom";
 import Overview from './pages/Overview/Overview';
+import theme from './theme';
 
 function App() {
     return (
-        <ChakraProvider>
+        <ChakraProvider theme={theme}>
             <Router>
 
 
-            <Navbar></Navbar>
+                <Navbar></Navbar>
 
-                        <Switch>
-          {/*<Route path="/about">*/}
-          {/*  <About />*/}
-          {/*</Route>*/}
-          {/*<Route path="/users">*/}
-          {/*  <Users />*/}
-          {/*</Route>*/}
-          <Route path="/">
-            <Overview />
-          </Route>
-        </Switch>
+                <Switch>
+                    {/*<Route path="/about">*/}
+                    {/*  <About />*/}
+                    {/*</Route>*/}
+                    {/*<Route path="/users">*/}
+                    {/*  <Users />*/}
+                    {/*</Route>*/}
+                    <Route path="/">
+                        <Link to={''}>Hello</Link>
+                        <Overview/>
+                    </Route>
+                </Switch>
             </Router>
         </ChakraProvider>
     );
--- front-end/src/theme.ts
+++ front-end/src/theme.ts
@@ -1,11 +1,44 @@
-// 1. import `extendTheme` function
-import { extendTheme } from "@chakra-ui/react"
-// 2. Add your color mode config
-const config = {
-  initialColorMode: "dark",
-  useSystemColorMode: false,
+import {extendTheme, ThemeConfig} from "@chakra-ui/react"
+import type {Styles} from '@chakra-ui/theme-tools';
+import {darken, mode, whiten} from "@chakra-ui/theme-tools";
+import {Dict} from '@chakra-ui/utils/dist/types/types';
+
+const config: ThemeConfig = {
+    initialColorMode: "dark",
+    useSystemColorMode: false,
 }
-// 3. extend the theme
-// @ts-ignore
-const theme = extendTheme({ config })
-export default theme
+
+const colors = {
+    primary: "#845EC2",
+    secondary: "#FF6F91",
+    highlight: "#00C9A7",
+    warning: "#FFC75F",
+    danger: "#C34A36",
+}
+
+const styles: Styles = {
+    global: (props: Dict) => ({
+        body: {
+            color: mode('gray.800', 'whiteAlpha.900')(props),
+            bg: mode('white', 'gray.900')(props)
+        }
+    })
+};
+
+const components = {
+    Link: {
+        baseStyle: (props: Dict) => ({
+            bg: mode(whiten("primary", 20), darken("primary", 20))(props),
+            color: props.colorMode === "dark" ? "gray.800" : "white",
+        }),
+    },
+}
+
+const theme = extendTheme({
+    config,
+    colors,
+    components,
+    styles
+});
+
+export default theme;
